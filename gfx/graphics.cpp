9a9a98f82d143f564fa80cca2213b050f6cd485b
samuel <samuel@ritlew.com>
2017-04-01 23:07:50 -0400

    comment and clean

--- gfx/graphics.cpp
+++ gfx/graphics.cpp
@@ -1,49 +1,65 @@
 #include <SFML/Graphics.hpp>
 #include "../SimpleIni/SimpleIni.hpp"
-#include <stdio.h>
 
-using namespace std;
-
+// Big graphics class
+// Manages the window
 class Graphics {
     private:
-        sf::RenderWindow window;
-        CSimpleIniA ini;
-        void createIniHandler(const char *);
-        void setTitleIcon(const char *);
+        // variables
+        sf::RenderWindow window; // actual window
+        CSimpleIniA ini; // ini file for initial values
+
+        // methods
+        void createIniHandler(const char *); // creates the ini file for use
+        void setTitleIcon(const char *); // sets the icon for the window
     public:
+        // methods
+        void loop(); // runs a refresh loop
+        bool running(); // returns whether the window is open or not
+
+        // constructor
         Graphics(const char *);
-        void loop();
-        bool running();
 };
 
 Graphics::Graphics(const char * iniFilename){
+    // create the ini file
     this->createIniHandler(iniFilename);
 
+    // get the needed variables
     int height = atoi(ini.GetValue("main", "WINDOW_HEIGHT", NULL));
     int width = atoi(ini.GetValue("main", "WINDOW_WIDTH", NULL));   
     const char * windowTitle = ini.GetValue("main", "WINDOW_TITLE", NULL);
     const char * iconPath = ini.GetValue("resources", "iconPath", NULL);
 
-
-    this->window.create(sf::VideoMode(height, width), "test");
+    // set up the window
+    this->window.create(sf::VideoMode(height, width), windowTitle);
     this->setTitleIcon(iconPath);
 }
 
+// this function creates the object for reading from the ini file
 void Graphics::createIniHandler(const char * iniFilename){
     SI_Error rc = this->ini.LoadFile(iniFilename);
     if (rc < 0){
         printf("Error with ini file\n");
+        exit(-1);
     }
 }
 
+// sets the title icon
 void Graphics::setTitleIcon(const char * filename){
+    // loads an image
     sf::Image image;
     image.loadFromFile(filename);
+
+    // puts them in the window
     this->window.setIcon(image.getSize().x, image.getSize().y, image.getPixelsPtr());
 }
 
+// runs a single refresh loop for the window
 void Graphics::loop(){
+    // make sure the window is open
     if (this->window.isOpen()){
+        // event loop
         sf::Event event;
         while (this->window.pollEvent(event))
         {
@@ -51,7 +67,7 @@ void Graphics::loop(){
                 this->window.close();
         }
 
-        this->window.clear();
+        // display
         this->window.display();
     }
 } 
