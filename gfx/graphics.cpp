314c68e08a7718d268d1489451fda5d7b50fbbbf
samuel <samuel@ritlew.com>
2017-04-01 21:38:36 -0400

    added ini file handler
    https://github.com/brofield/simpleini

--- gfx/graphics.cpp
+++ gfx/graphics.cpp
@@ -1,6 +1,12 @@
 #include <SFML/Graphics.hpp>
+#include "../SimpleIni/SimpleIni.hpp"
 
 int main(){
+    CSimpleIniA ini;
+    ini.SetUnicode();
+    ini.LoadFile("graphics.ini");
+
+
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
